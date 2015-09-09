59ca8368c2f66290a23189b218d16716d204fbe4
Martin Solli <msolli@gmail.com>
2015-09-09 22:24:05 +0200

    Make it into a regular @Component

--- src/main/java/no/smallinternet/universaljstodo/domain/TodoLoader.java
+++ src/main/java/no/smallinternet/universaljstodo/domain/TodoLoader.java
@@ -5,21 +5,16 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import static java.util.stream.Collectors.toList;
 
 @Component
-public class TodoLoader implements ApplicationListener<ContextRefreshedEvent> {
+public class TodoLoader {
     private static final Logger LOG = LoggerFactory.getLogger(TodoLoader.class);
 
     @Autowired
-    private TodoRepository repository;
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public TodoLoader(TodoRepository repository) {
         LOG.info("Loading Todo application data");
 
         String[] items = {"Turn on", "Tune in", "Drop out"};
