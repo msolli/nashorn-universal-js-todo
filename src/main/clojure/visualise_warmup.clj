d463f7445e0a643b012da903b17e5f3f6083e5f8
Martin Solli <msolli@gmail.com>
2015-09-09 22:24:42 +0200

    Label the axis

--- src/main/clojure/visualise_warmup.clj
+++ src/main/clojure/visualise_warmup.clj
@@ -1,23 +1,26 @@
+;; Shift - Cmd - L: Load file
+;; Shift - Cmd - P: Load top-level form under caret
+
 (ns visualise-warmup
     (:require
       [cl-java-introspector.spring :refer :all]
       [cl-java-introspector.core :refer :all]
       [alembic.still :refer [distill]]))
 
-(def renderer (get-bean "jsRenderer"))
-
 (use '(incanter core charts datasets))
 
+(def renderer (get-bean "jsRenderer"))
 (def stats (into [] (-> renderer .getLastWarmupResult .getStats)))
 
-(view (doto (xy-plot (range (length stats)) stats)
-            (set-axis :y (log-axis :base 10))))
+;(view (doto (xy-plot (range (length stats)) stats)
+;            (set-axis :y (log-axis :base 10))))
 
 (view (doto (xy-plot (range (length stats)) stats)
             (set-y-range 0 50)
-            ()))
+            (set-y-label "Time (ms)")
+            (set-x-label "Iterations")))
 
-(view (xy-plot (range (length stats)) stats))
+;(view (xy-plot (range (length stats)) stats))
 
-(view (doto (trace-plot stats)
-            (set-axis :y (log-axis :base 10))))
+;(view (doto (trace-plot stats)
+;            (set-axis :y (log-axis :base 10))))
